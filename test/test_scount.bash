167a871b1e83410b3b3eb94cfce9798bfbabf9b4
Takamasa Oshikiri <mail@oshikiri.org>
2019-08-25 11:07:08 +0900

    Update .travis.yml

--- test/test_scount.bash
+++ test/test_scount.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 expected='{"a":7,"b":2}'
-actual=$(cat ./demo/sample_data.txt | ./scount)
+actual=$(cat ./demo/sample_data.txt | scount)
 if [[ $actual != $expected ]]; then
   echo "sample_data.txt: expected ${expected} but got ${actual}"
   exit 1
