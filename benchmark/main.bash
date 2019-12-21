30d7d61548a55794e079be67abe6ac317baf0b53
Takamasa Oshikiri <mail@oshikiri.org>
2019-12-21 22:37:29 +0900

    Add benchmark script

---
+++ benchmark/main.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+n_trials=10
+
+git_version=$(git log -1 --pretty=%H)
+output_path=results/elasped-times.tsv
+
+function run () {
+  target_command=$1
+  for i in $(seq $n_trials); do
+    /usr/bin/time \
+      -f "$target_command\t$i\t%e" \
+      -o $output_path \
+      -a cat ../demo/text8_0 | eval $target_command > /dev/null 2>&1
+  done
+}
+
+echo 'command	i_trials	elapsed_time' > $output_path
+
+run 'scount -q'
+run 'scount -q -a'
+run 'scount'
+run 'scount -a'
+
+echo $git_version > ./results/environments.log
