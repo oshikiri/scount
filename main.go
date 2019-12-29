97d9616f5083cdf00d19c5a18e215be1e2f22dab
Takamasa Oshikiri <mail@oshikiri.org>
2019-12-29 10:49:59 +0900

    Replace options (e -> ae, s -> as)

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import "flag"
 func main() {
 	var topnPrint = flag.Int("n", 10, "Print the top N items")
 	var approximateCounting = flag.Bool("a", false, "Use approximate counting algorithm (default is naive counting)")
-	var approximateCountingEpsilon = flag.Float64("e", 1e-5, "Epsilon of lossy counting algorithm")
-	var approximateCountingSupport = flag.Float64("s", 2e-5, "Support of lossy counting algorithm")
+	var approximateCountingEpsilon = flag.Float64("ae", 1e-5, "Epsilon of lossy counting algorithm")
+	var approximateCountingSupport = flag.Float64("as", 2e-5, "Support of lossy counting algorithm")
 	var quietMode = flag.Bool("q", false, "Turn on quiet mode")
 	var flushMilliSec = flag.Int64("f", 200, "Flush counting progress every X [msec]")
 	flag.Parse()
