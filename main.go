85f12e1d2a41a5ca05e5e9839eccaca51f440817
Takamasa Oshikiri <mail@oshikiri.org>
2019-12-29 10:50:53 +0900

    Fix -q description

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ func main() {
 	var approximateCounting = flag.Bool("a", false, "Use approximate counting algorithm (default is naive counting)")
 	var approximateCountingEpsilon = flag.Float64("ae", 1e-5, "Epsilon of lossy counting algorithm")
 	var approximateCountingSupport = flag.Float64("as", 2e-5, "Support of lossy counting algorithm")
-	var quietMode = flag.Bool("q", false, "Turn on quiet mode")
+	var quietMode = flag.Bool("q", false, "Suppress a progress report")
 	var flushMilliSec = flag.Int64("f", 200, "Flush counting progress every X [msec]")
 	flag.Parse()
 
