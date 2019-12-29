201380d6053e283e4ae31cf9db866a6a6885c594
Takamasa Oshikiri <mail@oshikiri.org>
2019-12-29 22:28:17 +0900

    Fix gofmt issues

--- entries_test.go
+++ entries_test.go
@@ -4,9 +4,9 @@ import "testing"
 
 func TestEntryListLen(t *testing.T) {
 	entryList := []Entry{
-		Entry{"b", 20},
-		Entry{"a", 10},
-		Entry{"c", 5},
+		{"b", 20},
+		{"a", 10},
+		{"c", 5},
 	}
 	if len(entryList) != 3 {
 		t.Errorf("Unexpected length: len(%v) != 3", entryList)
@@ -17,9 +17,9 @@ func TestSortMap(t *testing.T) {
 	m := map[string]int{"a": 10, "b": 20, "c": 5}
 	actual := sortMap(m, 3)
 	expected := []Entry{
-		Entry{"b", 20},
-		Entry{"a", 10},
-		Entry{"c", 5},
+		{"b", 20},
+		{"a", 10},
+		{"c", 5},
 	}
 	if !equalEntryList(actual, expected) {
 		t.Errorf("Unexpected return value: %v != %v", actual, expected)
