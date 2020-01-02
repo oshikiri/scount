bc29997da5e780d781254f2a820211698042242e
Takamasa Oshikiri <mail@oshikiri.org>
2020-01-02 12:35:25 +0900

    Add memory_usage_kb column to benchmark results

--- benchmark/main.bash
+++ benchmark/main.bash
@@ -3,19 +3,19 @@
 n_trials=10
 
 git_version=$(git log -1 --pretty=%H)
-output_path=results/elasped-times.tsv
+output_path=results/benchmark_results.tsv
 
 function run () {
   target_command=$1
   for i in $(seq $n_trials); do
     /usr/bin/time \
-      -f "$target_command\t$i\t%e" \
+      -f "$target_command\t$i\t%e\t%M" \
       -o $output_path \
       -a cat ../demo/text8_0 | eval $target_command > /dev/null 2>&1
   done
 }
 
-echo 'command	i_trials	elapsed_time' > $output_path
+echo 'command	i_trials	elapsed_time	memory_usage_kb' > $output_path
 
 run 'scount -q'
 run 'scount -q -a'
